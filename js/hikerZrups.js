8308605da782e8c26397d60ccc421244a07a3f21
Leticia Olábarri <leticiaolabarri@yahoo.es>
2018-10-12 15:34:32 +0200

    player defined (he is called hikerZrups). It is not drawn in canvas, I do not know whay

--- js/hikerZrups.js
+++ js/hikerZrups.js
@@ -1,4 +1,26 @@
+window.onload = function() {
+
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
-function hikerZrups();
+var gravity = 3;
+
+var hikerZrups = {
+  x: 0,
+  y: 610,
+  width: 20,
+  height: 20,
+  speedX: 0,
+  speedY: gravity,
+  color: rgb(255,0,0),
+
+  draw: function() {
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+    return;
+  }
+};
+
+hikerZrups.draw();
+
+};
